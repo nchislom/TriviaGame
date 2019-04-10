a37d504138baef9efae089a77d3bef8aba5ab7ab
NChislom <11775893+nchislom@users.noreply.github.com>
2019-04-10 18:02:13 -0400

    Building out quiz JS object structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,48 @@
-// Quiz Object Schema
-// Question: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 4]
-// Key =    Question <string>
-// Value =  Array index 0-3 are possible choices; Array index 4 is the correct choice
 
-// var quiz = {
-//     'What is love!?': ['Baby', 'Don\'t', 'Hurt', 'Me', 3]
-// }
-
+var correctAnswers = 0;
 var quiz = {
     q1: {
-        question: 'What is love!?',
-        choice: ['Baby', 'Dont', 'Hurt', 'Me'],
-        answer: 'Baby'
+        question: 'CHOAM is an acronym for:',
+        choice: ['Chaperhouse Omni Aulos Merit', 'Cerebrus Hecate Ordos Mori', 'Combine Honnete Ober Advancer Mercantiles', 'Cows, Horses, Orcas, and Manitees'],
+        answer: 'Combine Honnete Ober Advancer Mercantiles'
+    },
+
+    q2: {
+        question: 'What is a mentat?',
+        choice: ['A unit of curreny', 'A human computer', 'A masculine tattoo', 'A unit of interstellar distance'],
+        answer: 'A human computer'
+    },
+
+    q3: {
+        question: 'What can spice (melange) be used for?',
+        choice: ['Extend life', 'Space travel', 'Expand conciousness', 'All of the above'],
+        answer: 'All of the above'
+    },
+
+    q4: {
+        question: 'What planet is the ancestral home of Duke Leto Atreides?',
+        choice: ['Caladan', 'Arrakis', 'Ix', 'Giedi Prime'],
+        answer: 'Caladan'
+    },
+
+    q5: {
+        question: 'In what year is Alia Atreides born?',
+        choice: ['88 BG', '1999 AD', '10193 BG', '10191 AG'],
+        answer: '10191 AG'
+    },
+
+    q6: {
+        question: 'The Litany Against ______ is used to help focus the mind in times of peril. (Fill in the blank)',
+        choice: ['Death', 'Fear', 'JQuery', 'Clarity'],
+        answer: 'Fear'
     }
 
     }
+var getScore = function() {
+    var score = Math.floor(correctAnswers/Object.keys(quiz).length*100).toString();
+    return score + '%';
+}
+
+var displayQuestion = function(num){
+    
+}
