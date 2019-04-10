f8ca366da4f1bfdd007ad34f407d1152fd15a2a9
NChislom <11775893+nchislom@users.noreply.github.com>
2019-04-09 23:20:01 -0400

    Initial Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Quiz Object Schema
+// Question: ['Choice 1', 'Choice 2', 'Choice 3', 'Choice 4', 4]
+// Key =    Question <string>
+// Value =  Array index 0-3 are possible choices; Array index 4 is the correct choice
+
+// var quiz = {
+//     'What is love!?': ['Baby', 'Don\'t', 'Hurt', 'Me', 3]
+// }
+
+var quiz = {
+    q1: {
+        question: 'What is love!?',
+        choice: ['Baby', 'Dont', 'Hurt', 'Me'],
+        answer: 'Baby'
+    }
+
+    }
